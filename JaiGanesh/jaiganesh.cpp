428aa75522c6c39e8213f6b9f2376e8987daa9eb
khushank11 <154746385+khushank11@users.noreply.github.com>
2024-03-02 22:45:12 +0530

    Initial commit

---
+++ JaiGanesh/jaiganesh.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    cout<<"jai ganesh";
+
+    return 0;
+}
---
+++ Patterns/Square/pattern_01.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    int n;
+    cout<<"enter size:";
+    cin>>n;
+
+    int i=1;
+    for(;i<=n;i+=1){
+        for(int j=1;j<=n;j++){
+            cout<<"*";
+
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
